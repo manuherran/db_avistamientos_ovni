48e9d3eba38803607c8ca1ccc6345f463e325a87
Manu Herrán <139716025+manuherran@users.noreply.github.com>
2026-03-18 17:52:45 +0100

    Add files via upload

--- db_avistamientos_ovni_consultas_1.sql
+++ db_avistamientos_ovni_consultas_1.sql
@@ -6,4 +6,16 @@ SELECT DISTINCT id_incidente FROM t_testimonio;
 SELECT COUNT(DISTINCT id_testimonio) FROM t_testimonio;
 SELECT COUNT(DISTINCT id_incidente) FROM t_testimonio;
 
--- Número de valores únicos
+
+-- Número de testimonios por cada testimonios: obviamente es uno
+SELECT id_testimonio, COUNT(*) FROM t_testimonio
+GROUP BY id_testimonio ORDER BY COUNT(*) DESC;
+
+-- Número de testimonios por cada incidente
+SELECT id_incidente, COUNT(*) FROM t_testimonio
+GROUP BY id_incidente ORDER BY COUNT(*) DESC;
+
+-- Número de valores únicos (incidentes que tienen solo un testimonio)
+SELECT id_testimonio FROM t_testimonio GROUP BY id_testimonio HAVING COUNT(*) = 1;
+SELECT id_incidente FROM t_testimonio GROUP BY id_incidente HAVING COUNT(*) = 1;
+
