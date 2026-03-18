e7acc37e68463cab2113c7d348c3f76a412ae544
Manu Herrán <139716025+manuherran@users.noreply.github.com>
2026-03-18 17:45:58 +0100

    Add files via upload

--- db_avistamientos_ovni_consultas_1.sql
+++ db_avistamientos_ovni_consultas_1.sql
@@ -1,9 +1,9 @@
 -- Número de valores diferentes (lista de valores únicos)
-SELECT DISTINCT id_avistamiento FROM t_testimonio;
-SELECT COUNT(DISTINCT id_avistamiento) FROM t_testimonio;
+SELECT DISTINCT id_testimonio FROM t_testimonio;
+SELECT DISTINCT id_incidente FROM t_testimonio;
 
 -- Número de valores diferentes (cuenta los valores únicos)
-SELECT DISTINCT id_incidente FROM t_testimonio;
+SELECT COUNT(DISTINCT id_testimonio) FROM t_testimonio;
 SELECT COUNT(DISTINCT id_incidente) FROM t_testimonio;
 
 -- Número de valores únicos
